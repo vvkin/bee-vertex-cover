a5f5df8969b3f977eac58c3cedfb516fe69072f3
Vadym Kinchur <vkincur@gmail.com>
2020-10-04 21:48:58 +0300

    Optimize graph generation, add networkx to Pipfile

--- generate_graph.py
+++ generate_graph.py
@@ -2,47 +2,27 @@
 
 """
 Script to generate random undirected graph
-with given number of vertices and edges.
-Power of each vertex ∈ [2, max_power] (if there are enough edges)
-Usage: python generate_graph.py [vertex_num] [edges_num] [max_power] [file_name]
+with given number of vertices and expected power of each vertex.
+Power of each vertex ∈ [0, max_power] (if there are enough edges)
+Usage: python generate_graph.py [vertex_num] [max_power] [file_name]
 """
 
 import numpy as np
+import networkx as nx
 import sys
 
-class TooSmallPower(Exception): pass # just custom Exception to handle errors with mask
-
 if __name__ == '__main__':
-    if len(sys.argv) != 5:
-        sys.exit('Incorrect number of parameters')
-    
     try:
-        v_num, e_num, max_power = map(np.int, sys.argv[1:4])
-        file_handler = open(sys.argv[4], 'w')
+        v_num, max_power = map(np.int, sys.argv[1:3])
+        file_name = sys.argv[3]
         
-        powers = np.zeros(v_num, np.int) # degree of each vertex
-        vertices = np.arange(v_num) # just range
- 
-        file_handler.write(f'{v_num} {e_num}\n')
+        degrees = np.random.randint(2, max_power+1, size=v_num) # random degree of each vertex
+        graph = nx.generators.degree_seq.expected_degree_graph(degrees, selfloops=False)
+        np.save(file_name, nx.to_numpy_matrix(graph))
 
-        for _ in range(e_num):
-            mask = powers < 2 # vertices with power less than two
-            mask = powers < max_power if not np.any(mask) else mask # else select random vertices
-            if np.count_nonzero(mask) < 2: raise TooSmallPower # here are at least two valid vertices
-            
-            src = np.random.choice(vertices[mask])
-            mask[src] = False  # prevent (src == dest)
-            dest = np.random.choice(vertices[mask])
-            powers[src] += 1; powers[dest] += 1
-
-            file_handler.write(f'{src} {dest}\n')
-            
-        file_handler.close()
-    
-    except TooSmallPower:
-        file_handler.close()
-        sys.exit('Too small maximal power of each vertex!')
+    except IndexError:
+        sys.exit('Incorrect number of input parameters')
     except ValueError:
-        sys.exit('Incorrect type of input parameters')
+        sys.exit('Invalid input parameters')
     except:
         sys.exit('Sorry, but something went wrong')
